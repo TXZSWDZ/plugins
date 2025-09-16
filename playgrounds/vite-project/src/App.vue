e1f0a0a0fd57a201a33a6f37ad6ef0a94d1ae375
WangZe <wangze@zb-software.com>
2025-09-16 15:39:59 +0800

    feat：添加测试组件

--- playgrounds/vite-project/src/App.vue
+++ playgrounds/vite-project/src/App.vue
@@ -1,4 +1,11 @@
 <template>
-  App
+  {{ name }}
+  <Test />
 </template>
+<script setup name="App">
+  import { ref } from 'vue'
+  import  Test from "./components/test.vue"
+
+  const name = ref('App')
+</script>
 
---
+++ playgrounds/vite-project/src/components/test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{ message }}
+    </div>
+</template>
+
+<script setup name="Test">
+    defineOptions({
+        inheritAttrs: false
+    });
+    import { ref } from 'vue'
+    
+    const message = ref('Hello World!')
+</script>
